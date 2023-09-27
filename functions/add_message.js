e56571d5b774f23f00810d81e9d07a20bcd0f50f
AhmadElsaeed <75431123+AhmadElsaeedd@users.noreply.github.com>
2023-09-27 15:46:11 +0400

    implemented chat functionality fully

--- functions/add_message.js
+++ functions/add_message.js
@@ -12,16 +12,34 @@ const corsOptions = {
   origin: true,
 };
 
-async function add_message_to_chat(chat_id, uid, content) {
+async function add_message_to_chat(chat_id, new_message) {
   let done = false;
-  const timestamp = new Date();
-  await db.collection("Matches").doc(chat_id).collection("Messages").add({
-    sent_by: uid,
-    content: content,
-    timestamp: timestamp,
-  });
+  await db.collection("Matches").doc(chat_id).collection("Messages").add(new_message);
   done = true;
   return done;
+}
+
+async function get_match_data(match_id) {
+  const match_doc = await (db.collection("Matches").doc(match_id)).get();
+  return match_doc.data();
+}
+
+async function update_match_doc(match_data, chat_id, new_message) {
+  let done2 = false;
+  if (match_data.has_message === true) {
+    // make has_message true and update the last message
+    await db.collection("Matches").doc(chat_id).update({
+      last_message: new_message,
+    });
+  } else {
+    // has_message is already true, update the last message
+    await db.collection("Matches").doc(chat_id).update({
+      has_message: true,
+      last_message: new_message,
+    });
+  }
+  done2 = true;
+  return done2;
 }
 
 const AddMessage = functions.region("asia-east2").https.onRequest(async (req, res) => {
@@ -30,12 +48,35 @@ const AddMessage = functions.region("asia-east2").https.onRequest(async (req, re
     const chat_id = req.body.chat_id;
     const content = req.body.content;
 
-    const done = await add_message_to_chat(chat_id, user_uid, content);
+    const timestamp = new Date();
 
-    if (done) {
-      res.status(200).send("Message sent");
+    const new_message = {
+      sent_by: user_uid,
+      content: content,
+      timestamp: timestamp,
+    };
+
+    // Start Process 2: add_message_to_chat
+    const process1 = add_message_to_chat(chat_id, new_message);
+
+    // Start Process 1: get_match_data => update_match_doc
+    const process2 = get_match_data(chat_id)
+        .then((match_data) => update_match_doc(match_data, chat_id, new_message))
+        .catch((error) => console.error("Error in Process 1:", error));
+
+    try {
+      // Wait for both processes to complete
+      const [done, done2] = await Promise.all([process1, process2]);
+
+      if (done && done2) {
+        res.status(200).send("Message sent");
+      }
+    } catch (error) {
+      console.error("Error occurred:", error);
+      res.status(500).send("Internal Server Error");
     }
   });
 });
+
 
 module.exports = AddMessage;
